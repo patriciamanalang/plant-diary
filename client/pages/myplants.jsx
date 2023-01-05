d1f16ed1cbaaeb556a6463cd46e993aa93c44222
Patricia Manalang <patriciamanalang06@gmail.com>
2023-01-05 23:48:32 +0000

    completed feature 1.

--- client/pages/myplants.jsx
+++ client/pages/myplants.jsx
@@ -31,16 +31,16 @@ export default class MyPlants extends React.Component {
         <div className='row'>
           <div className='my-plants'>
             <h1 className='header'>My Plants</h1>
-            <button onClick={this.handleButtonClick} className='add-plant'>Add a plant +</button>
+            <button type="button" onClick={this.handleButtonClick} className='add-plant'>Add a plant +</button>
             <h4 className='hidden no-plant'>You have no plants on your plant list!</h4>
-            <div className='plantnames-container'>
-              <h3 className=' plantname'>Philodendron Gloriosum</h3>
+            <div className='hidden plantnames-container'>
+              <h3 className=' hidden plantname'>Philodendron Gloriosum</h3>
               <i className="fa-solid fa-trash" />
             </div>
           </div>
         </div>
-        <div className='delete-plant-modal'/>
-        <div className='delete-plant-rectangle' />
+        <div className='hidden delete-plant-modal'/>
+        <div className='hidden delete-plant-rectangle' />
 
       </div>
 
