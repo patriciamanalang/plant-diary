908045f1ab1a756e8475bcb870c8fc1f08dc86f9
Patricia Manalang <patriciamanalang06@gmail.com>
2023-02-01 03:32:47 +0000

    styling completed

--- client/pages/add-entry.jsx
+++ client/pages/add-entry.jsx
@@ -36,10 +36,22 @@ export default class AddEntry extends React.Component {
             <h1 className='plantentry-name'>Snake Plant</h1>
           </div>
         </div>
-        <div className='column-full'>
-          <div className='row'>
-            <div className='plant-photo-div'>
-              <img className='plant-photo' src="https://www.wolflair.com/wp-content/uploads/2017/01/placeholder-300x300.jpg" alt="placeholder image" />
+        <div className='column-full '>
+          <div className='row plant-photo-div'>
+            <img className='plant-photo' src="https://www.wolflair.com/wp-content/uploads/2017/01/placeholder-300x300.jpg" alt="placeholder image" />
+          </div>
+          <div className='add-photo-div'>
+            <button className='add-photo'>Add Photo</button>
+          </div>
+          <div className='notes-div'>
+            <label className='notes-label' htmlFor="notes">Notes</label>
+            <div className='notes'>
+              <textarea name="notes" id="notes" cols="34" rows="10" />
+            </div>
+          </div>
+          <div className='column-full'>
+            <div className='save-btn-div'>
+              <button className='save-btn'>Save</button>
             </div>
           </div>
         </div>
